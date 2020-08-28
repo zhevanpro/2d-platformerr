524e04c75b1e1fb64194342538d0f9ffdf68f87f
zhevan <prozhevan@gmail.com>
2020-08-28 10:18:37 +0700

    Wes import unity standard asset coding mlaku wes mari tapi gaiso mlaku

---
+++ source/Assets/Script/player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D myRigidBody;
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        run();
+    }
+
+    private void run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+    }
+}
+
